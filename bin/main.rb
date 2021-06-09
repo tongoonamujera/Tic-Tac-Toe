d1ea91d9fc3c184b76e95bc43f3adb1af3e35b6c
tongoonamujera <tongoonamujera@gmail.com>
2021-06-09 15:39:09 +0200

    added more methods to Game class in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 class Game
-  attr_accessor :first_player, :second_player, :players
+  attr_accessor :first_player, :second_player, :game_players
 
   def initialize
     @first_player = nil
@@ -29,10 +29,10 @@ class Game
     puts 
     puts "Enter First Player's name: "
     @first_player = gets.chomp()
-    @players[O] = @first_player
+    @game_players[O] = @first_player
     puts "Enter Second Player's name: "
     @second_player = gets.chomp()
-    @players[X] = @second_player
+    @game_players[X] = @second_player
     sleep 3
     puts "#{@first_player} is O and #{@second_player} is X"
     sleep 3
@@ -41,4 +41,48 @@ class Game
     puts
     sleep 3
   end
+
+  def main_game_section
+    @game_players.values.each do |current_player|
+      draw_game_board
+      puts
+      puts "Hey #{current_player}!, it's your turn"
+      puts "Please select a cell available on the board"
+      game_logic_section(current_player)
+    end
+  end
+
+  def game_logic_section(current_player)
+    loop do
+      unless @game_counter < 9
+        @game_winner = @first_player
+        @game_ended = true
+        return nil
+      else
+        player_choice = gets.chomp.to_i
+        unless @available_choices.include?(player_choice)
+          @game_counter += 1
+          @available_choices[player_choice - 1] = @game_players.key(current_player)
+          break
+        else
+          puts "Invalid entry, #{current_player}! please try again... (your input must be a number between 1 and 9)"
+        end
+      end
+    end
+  end
+
+  def start_game
+    game_start_section
+    main_game_section until @game_ended
+    unless @game_winner == "TON"
+      puts "#{@game_winner} is winner! "
+    else
+      puts "TON is the winner"
+    end
+    puts
+    puts "Its game Over!"
+  end
 end
+
+game = Game.new
+game.start_game
