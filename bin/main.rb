4ea96ec0d78f5b93e8b62a6e88932d416a60b1b2
tongoonamujera <tongoonamujera@gmail.com>
2021-06-09 13:35:50 +0200

    added some few logic of the game

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,25 @@
 #!/usr/bin/env ruby
 
-puts 'Hello'
+class Game
+  attr_accessor :first_player, :second_player, :players
+
+  def initialize
+    @first_player = nil
+    @second_player = nil
+    @players = []
+    @available_choices = Array(1..9)
+    @game_ended = false
+    @game_counter = 0
+    @game_winner = "TON"
+  end
+
+  def draw_game_board
+    puts '+---+---+---+'
+    puts "| #{@available_choices[0]} | #{@available_choices[1]} | #{@available_choices[2]} |"
+    puts '+---+---+---+'
+    puts "| #{@available_choices[3]} | #{@available_choices[4]} | #{@available_choices[5]} |"
+    puts '+---+---+---+'
+    puts "| #{@available_choices[6]} | #{@available_choices[7]} | #{@available_choices[8]} |"
+    puts '+---+---+---+'
+  end
+end
